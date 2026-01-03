cf8f16b0e7f0b1318013305500904ff701f08172
Mauricio <mauricio_cy@hotmail.com>
2026-01-03 18:36:34 -0300

    feat: implement people_repository

---
+++ src/models/sqlite/repositories/people_repository.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm.exc import NoResultFound
+from src.models.sqlite.entities.people import People
+from src.models.sqlite.entities.pets import Pet
+
+
+class PeopleRepository:
+    def __init__(self, db_connection) -> None:
+        self.db_connection = db_connection
+
+    def insert_person(
+        self, first_name: str, last_name: str, age: int, pet_id: int
+    ) -> None:
+        with self.db_connection as db:
+            try:
+                new_person = People(
+                    first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
+                )
+                db.session.add(new_person)
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                raise e
+
+    def get_person(self, person_id: int) -> People:
+        with self.db_connection as db:
+            try:
+                person = (
+                    db.session.query(People)
+                    .outerjoin(Pet, Pet.id == People.pet_id)
+                    .filter(People.id == person_id)
+                    .with_entities(
+                        People.first_name,
+                        People.last_name,
+                        Pet.name.label("pet_name"),
+                        Pet.type.label("pet_type"),
+                    )
+                    .one()
+                )
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 # db_connection_handler.connect_to_db()
 
@@ -19,3 +20,27 @@ def test_delete_pets():
         repo.delete_pets("Buddy")
     except Exception as e:
         pytest.fail(f"delete_pets raised an exception: {e}")
+
+
+@pytest.mark.skip(reason="Test requires database setup")
+def test_insert_person():
+    first_name = "John"
+    last_name = "Doe"
+    age = 30
+    pet_id = 1
+    repo = PeopleRepository(db_connection_handler)
+    try:
+        repo.insert_person(first_name, last_name, age, pet_id)
+    except Exception as e:
+        pytest.fail(f"insert_person raised an exception: {e}")
+
+
+@pytest.mark.skip(reason="Test requires database setup")
+def test_get_person():
+    person_id = 1
+    repo = PeopleRepository(db_connection_handler)
+    try:
+        person = repo.get_person(person_id)
+        print(person)
+    except Exception as e:
+        pytest.fail(f"get_person raised an exception: {e}")
