2112ed79570c830519297517b0ba686b071883f3
Mauricio <mauricio_cy@hotmail.com>
2026-01-07 20:50:26 -0300

    feat: implement interfaces in controllers

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,22 +1,23 @@
 import re
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
+from .interfaces.person_creator_controller import PersonCreatorControllerInterface
 
 
-class PersonCreatorController:
+class PersonCreatorController(PersonCreatorControllerInterface):
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
 
-    def create(self, person_data: dict) -> dict:
-        first_name = person_data["first_name"]
-        last_name = person_data["last_name"]
-        age = person_data["age"]
-        pet_id = person_data["pet_id"]
+    def create(self, person_info: dict) -> dict:
+        first_name = person_info["first_name"]
+        last_name = person_info["last_name"]
+        age = person_info["age"]
+        pet_id = person_info["pet_id"]
 
         self.__validate_first_and_last_name(first_name, last_name)
         self.__validate_age(age)
         self.__insert_person_in_db(first_name, last_name, age, pet_id)
 
-        formatted_response = self.__format_response(person_data)
+        formatted_response = self.__format_response(person_info)
         return formatted_response
 
     def __validate_first_and_last_name(self, first_name: str, last_name: str) -> None:
@@ -40,11 +41,11 @@ class PersonCreatorController:
             first_name=first_name, last_name=last_name, age=age, pet_id=pet_id
         )
 
-    def __format_response(self, person_data: dict) -> dict:
+    def __format_response(self, person_info: dict) -> dict:
         return {
             "data": {
                 "type": "person",
                 "count": 1,
-                "attributes": person_data,
+                "attributes": person_info,
             }
         }
--- src/controllers/pet_lister_controller.py
+++ src/controllers/pet_lister_controller.py
@@ -1,8 +1,9 @@
 from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 from src.models.sqlite.entities.pets import Pet
+from .interfaces.pet_lister_controller import PetListerControllerInterface
 
 
-class PetListerController:
+class PetListerController(PetListerControllerInterface):
     def __init__(self, pets_repository: PetsRepositoryInterface) -> None:
         self.pets_repository = pets_repository
 
